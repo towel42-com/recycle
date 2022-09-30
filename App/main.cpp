fcd35677fe019ed2f13a85492f27ab9cd962b690
Scott Aron Bloom <scott@towel42.com>
2022-09-30 20:05:27 +0100

    Initial version

---
+++ App/main.cpp
@@ -0,0 +1,58 @@
+// The MIT License( MIT )
+//
+// Copyright( c ) 2020-2021 Scott Aron Bloom
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files( the "Software" ), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sub-license, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions :
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+#include "Version.h"
+#include "SABUtils/FileUtils.h"
+
+#include <QCoreApplication>
+#include <QCommandLineParser>
+#include <QDir>
+#include <QDebug>
+
+int main( int argc, char ** argv )
+{
+    QCoreApplication appl( argc, argv );
+    appl.setApplicationName( QString::fromStdString( NVersion::APP_NAME ) );
+    appl.setApplicationVersion( QString::fromStdString( NVersion::getVersionString( true ) ) );
+    appl.setOrganizationName( QString::fromStdString( NVersion::VENDOR ) );
+    appl.setOrganizationDomain( QString::fromStdString( NVersion::HOMEPAGE ) );
+
+    auto options = std::make_shared< NSABUtils::NFileUtils::SRecycleOptions >();
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription( "Recycler" );
+    auto help = parser.addHelpOption();
+    auto version = parser.addVersionOption();
+
+    QCommandLineOption deleteOnRecycle( QStringList() << "delete_on_recycle_failure", "Delete on recycle failure", "" );
+    parser.addOption( deleteOnRecycle );
+
+    parser.addPositionalArgument( "", "Paths to recycle" );
+    parser.process( appl );
+
+    options->fDeleteOnRecycleFailure = parser.isSet( deleteOnRecycle );
+    auto args = parser.positionalArguments();
+    for ( auto && ii : args )
+        NSABUtils::NFileUtils::moveToTrash( ii, options );
+
+    return 0;
+}
