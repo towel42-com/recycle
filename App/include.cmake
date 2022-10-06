af42b269be5cdfeace13f7ae0add12879f61818a
Scott Aron Bloom <scott@towel42.com>
2022-10-06 10:36:31 -0700

    Dont build SABUtils

--- App/include.cmake
+++ App/include.cmake
@@ -1,11 +1,22 @@
+IF(WIN32)
+        set( OS_SRCS ${CMAKE_SOURCE_DIR}/SABUtils/MoveToTrash_win.cpp)
+ELSE()
+        set( OS_SRCS ${CMAKE_SOURCE_DIR}/SABUtils/MoveToTrash_linux.cpp)
+ENDIF()
+
+
 set(qtproject_SRCS
-    main.cpp    
+    main.cpp
+    ${CMAKE_SOURCE_DIR}/SABUtils/MoveToTrash.cpp
+    ${OS_SRCS}
 )
 
 set(qtproject_H
+
 )
 
 set(project_H
+    ${CMAKE_SOURCE_DIR}/SABUtils/MoveToTrash.h
 )
 
 set(qtproject_UIS
@@ -16,5 +27,4 @@ set(qtproject_QRC
 )
 
 set( project_pub_DEPS
-    SABUtils
 )
